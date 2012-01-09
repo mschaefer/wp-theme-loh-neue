32460c1d5bfe23a1667df847c703a33f355f4c33
Matt Schaefer <matt@steelplatesahead.com>
2012-01-08 21:06:07 -0500

    initial commit of base theme files

---
+++ header.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8" />
+  <title></title>
+
+  <link rel="stylesheet" media="all" href="/css/base.css" />
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+
+  <!--[if lt IE 9]>
+  <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+
+  <!--
+  <link rel="shortcut icon" href="/css/images/favicon.ico">
+  <link rel="apple-touch-icon" href="/css/images/apple-touch-icon.png">
+  -->
+</head>
+
+<body>

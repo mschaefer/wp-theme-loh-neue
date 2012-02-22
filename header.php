344bd5ad0b0cd22b08af9882040b338a1875bc83
Matt Schaefer <matt@steelplatesahead.com>
2012-02-21 20:23:07 -0500

    favicon

--- header.php
+++ header.php
@@ -28,6 +28,8 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="initial-scale=1.0,width=device-width" />
 
+  <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri() ?>/css/images/favicon.ico">
+  <link rel="apple-touch-icon" href="<?php echo get_stylesheet_directory_uri() ?>/css/images/apple-touch-icon.png">
   <?php wp_head(); ?>
 </head>
 

ff0d0776f2e5819323f4097603dcd831ad233ec9
Matt Schaefer <matt@steelplatesahead.com>
2012-02-26 14:09:21 -0500

    added dynamic sidebar to posts sidebar

--- functions/dynamic_sidebars.php
+++ functions/dynamic_sidebars.php
@@ -1,11 +1,18 @@
 <?php
   if ( function_exists('register_sidebar') ) {
     register_sidebar( array('name' => 'Footer Credits') );
+    register_sidebar( array('name' => 'Posts Sidebar') );
   }
 
   function loh_footer_content() {
-    if( !dynamic_sidebar() ) {
+    if( !dynamic_sidebar('Footer Credits') ) {
       echo '';
     }
+  }
+
+  function loh_posts_sidebar() {
+    if( !dynamic_sidebar('Posts Sidebar') ) {
+      echo '';
+    } 
   }
 ?>
